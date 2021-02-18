b2d67f116c1afe8dbbf266ac6912ecc5b756d8cb
James Roach <roachjweston@gmail.com>
2021-02-18 13:17:54 -0800

    user can turn car

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+var $car = document.getElementById('car');
+
 function turnCar(event) {
   if (event.code === 'ArrowRight' || event.code === 'KeyD') {
-    console.log('Right');
+    if ($car.className === 'car') {
+      $car.className = 'car r90';
+    } else if ($car.className === 'car r90') {
+      $car.className = 'car r180';
+    } else if ($car.className === 'car r180') {
+      $car.className = 'car r270';
+    } else {
+      $car.className = 'car';
+    }
   } else if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
-    console.log('Left');
-  } else {
-    console.log(event);
+    if ($car.className === 'car') {
+      $car.className = 'car r270';
+    } else if ($car.className === 'car r270') {
+      $car.className = 'car r180';
+    } else if ($car.className === 'car r180') {
+      $car.className = 'car r90';
+    } else {
+      $car.className = 'car';
+    }
   }
 }
 
