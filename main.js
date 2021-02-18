1dc582cb99701f748ee87cbe4a202448296c069a
James Roach <roachjweston@gmail.com>
2021-02-18 13:24:49 -0800

    simplified js code

--- main.js
+++ main.js
@@ -2,25 +2,13 @@ var $car = document.getElementById('car');
 
 function turnCar(event) {
   if (event.code === 'ArrowRight' || event.code === 'KeyD') {
-    if ($car.className === 'car') {
-      $car.className = 'car r90';
-    } else if ($car.className === 'car r90') {
-      $car.className = 'car r180';
-    } else if ($car.className === 'car r180') {
-      $car.className = 'car r270';
-    } else {
-      $car.className = 'car';
-    }
+    $car.className = 'car';
+  } else if (event.code === 'ArrowUp' || event.code === 'KeyW') {
+    $car.className = 'car r270';
   } else if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
-    if ($car.className === 'car') {
-      $car.className = 'car r270';
-    } else if ($car.className === 'car r270') {
-      $car.className = 'car r180';
-    } else if ($car.className === 'car r180') {
-      $car.className = 'car r90';
-    } else {
-      $car.className = 'car';
-    }
+    $car.className = 'car r180';
+  } else if (event.code === 'ArrowDown' || event.code === 'KeyS') {
+    $car.className = 'car r90';
   }
 }
 
